3b49db9d3ac7f26dac57990c01c0fd14edfc31b2
Savalance98 <57797316+Savalance98@users.noreply.github.com>
2020-10-22 14:36:17 +0300

    Add files via upload

--- remove_palindroms.py
+++ remove_palindroms.py
@@ -1,13 +1,13 @@
 def remove_palindroms(spells):
     t = ''
     i = 0
-    while i < len(spells):
+    while i < len( spells ):
         p = spells[i].split()
-        p = ''.join(p)
-        for j in range(len(p) - 1, -1, -1):
+        p = ''.join( p )
+        for j in range( len( p ) - 1, -1, -1 ):
             t += p[j].lower()
-        if str(p).lower() == t:
-            spells.pop(i)
+        if str( p ).lower() == t:
+            spells.pop( i )
         else:
             i += 1
         t = ''
