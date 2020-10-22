ff8c8cd6c93753b4c06039dbb15d0ec938271b03
Savalance98 <57797316+Savalance98@users.noreply.github.com>
2020-10-22 14:26:05 +0300

    Add files via upload

---
+++ remove_palindroms.py
@@ -0,0 +1,14 @@
+def remove_palindroms(spells):
+    t = ''
+    i = 0
+    while i < len(spells):
+        p = spells[i].split()
+        p = ''.join(p)
+        for j in range(len(p) - 1, -1, -1):
+            t += p[j].lower()
+        if str(p).lower() == t:
+            spells.pop(i)
+        else:
+            i += 1
+        t = ''
+    return spells
